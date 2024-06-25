8d2b567fc5a0e5e1201709a3d5fadf2aa8227d6c
Your-Spy <148842305+Your-Spy@users.noreply.github.com>
2024-06-25 19:49:43 +0530

    Added ox_target export

--- [spy-bodycam]/spy-bodycam/client/target.lua
+++ [spy-bodycam]/spy-bodycam/client/target.lua
@@ -1,32 +1,64 @@
 CreateThread(function()
-    for k,v in ipairs(Config.WatchLoc) do
-		exports['qb-target']:AddCircleZone("spycam_watch"..k,v.coords,v.rad, { 
-			name = "spycam_watch"..k,
-			useZ=true,
-			debugPoly = v.debug,
-		}, {
-			options = {
-				{
-					type = "client",
-					icon = "fas fa-sign-in-alt",
-					label = 'Open Bodycam Portal',
-					action = function(entity)
-						TriggerEvent('spy-bodycam:openActiveMenu',k) 
-					  end,
-					canInteract = function(entity)
-						return isLocFilterTrue(k,PlayerJob.name)
-					end,
-					
+	if Config.Dependency.UseTarget == 'qb' then
+		for k,v in ipairs(Config.WatchLoc) do
+			exports['qb-target']:AddCircleZone("spycam_watch"..k,v.coords,v.rad, { 
+				name = "spycam_watch"..k,
+				useZ=true,
+				debugPoly = v.debug,
+			}, {
+				options = {
+					{
+						type = "client",
+						icon = "fas fa-sign-in-alt",
+						label = 'Open Bodycam Portal',
+						action = function(entity)
+							TriggerEvent('spy-bodycam:openActiveMenu',k) 
+						  end,
+						canInteract = function(entity)
+							return isLocFilterTrue(k,PlayerJob.name)
+						end,
+						
+					},
 				},
-			},
-			distance = 2.5
-		 })
+				distance = 2.5
+			 })
+		end 
+	else
+		for k,v in ipairs(Config.WatchLoc) do
+			exports.ox_target:addSphereZone(
+				{
+					coords = v.coords,
+					radius = v.rad,
+					debug = v.debug,
+					drawSprite = false,
+					options = {
+						name = "spycam_watch"..k,
+						label = 'Open Bodycam Portal',
+						icon = "fas fa-sign-in-alt",
+						distance = 2.5,
+						onSelect = function(data)
+							TriggerEvent('spy-bodycam:openActiveMenu',k) 
+						end,
+						canInteract = function(entity, distance, coords, name, bone)
+							return isLocFilterTrue(k,PlayerJob.name)
+						end,
+					}
+				}
+			)
+		end 
 	end
 end)
+
 AddEventHandler('onResourceStop', function(resourceName) 
     if GetCurrentResourceName() == resourceName then
-        for k,v in ipairs(Config.WatchLoc) do
-            exports['qb-target']:RemoveZone("spycam_watch"..k)
-        end
+		if Config.Dependency.UseTarget == 'qb' then
+			for k,v in ipairs(Config.WatchLoc) do
+				exports['qb-target']:RemoveZone("spycam_watch"..k)
+			end
+		else
+			for k,v in ipairs(Config.WatchLoc) do
+				exports.ox_target:removeZone("spycam_watch"..k)
+			end
+		end
     end 
 end)
